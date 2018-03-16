779d6486584d7b8089e8b6fe6d62cd62f59a2374
Eric Botter <eric.botter@gmtech.ch>
2018-03-16 09:35:50 +0100

    Added Replace Array with Object

---
+++ refactoring/src/refactoring/replacearraywithobject/Main.java
@@ -0,0 +1,18 @@
+package refactoring.replacearraywithobject;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Performance row = new Performance();
+		
+		row.setName("Liverpool");
+		row.setWins(15);
+
+		String name = row.getName();
+		int wins = row.getWins();
+		
+		System.out.println(name);
+		System.out.println(wins);
+	}
+
+}
---
+++ refactoring/src/refactoring/replacearraywithobject/Performance.java
@@ -0,0 +1,22 @@
+package refactoring.replacearraywithobject;
+
+public class Performance {
+	private String _name;
+	private int _wins;
+	
+	public String getName() {
+		return _name;
+	}
+
+	public void setName(String name) {
+		_name = name;
+	}
+	
+	public int getWins() {
+		return _wins;
+	}
+	
+	public void setWins(int wins) {
+		_wins = wins;
+	}
+}
