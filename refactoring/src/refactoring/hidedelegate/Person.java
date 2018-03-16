a22d6ed59a35630328d318e097fd1ce000d776b4
Eric Botter <eric.botter@gmtech.ch>
2018-03-16 15:29:44 +0100

    After Hide Delegate

--- refactoring/src/refactoring/hidedelegate/Person.java
+++ refactoring/src/refactoring/hidedelegate/Person.java
@@ -3,13 +3,11 @@ package refactoring.hidedelegate;
 public class Person {
 	private Department _department;
 
-	public Department getDepartment() {
-		return _department;
+	public Person getManager() {
+		return _department.getManager();
 	}
 
 	public void setDepartment(Department department) {
 		_department = department;
 	}
-	
-	
 }
