1b73e0e43c16ed1447ccc325d2dc268f213b45f4
Eric Botter <eric.botter@gmtech.ch>
2018-03-22 08:57:52 +0100

    After Replace Inheritance with Delegation

--- refactoring/src/refactoring/replace_inheritance_with_delegation/MyStack.java
+++ refactoring/src/refactoring/replace_inheritance_with_delegation/MyStack.java
@@ -2,14 +2,24 @@ package refactoring.replace_inheritance_with_delegation;
 
 import java.util.Vector;
 
-public class MyStack extends Vector {
+public class MyStack {
+	private final Vector _vector = new Vector();
+	
 	public void push(Object element) {
-		insertElementAt(element, 0);
+		_vector.insertElementAt(element, 0);
 	}
 	
 	public Object pop() {
-		Object result = firstElement();
-		removeElementAt(0);
+		Object result = _vector.firstElement();
+		_vector.removeElementAt(0);
 		return result;
+	}
+	
+	public int size() {
+		return _vector.size();
+	}
+	
+	public boolean isEmpty() {
+		return _vector.isEmpty();
 	}
 }
